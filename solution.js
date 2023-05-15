161e6c96345426aad0c2ffa08e064a14ba2e4d6f
Laura Wu <leemaiwu@gmail.com>
2023-05-15 11:11:26 -0600

    Fixed ternary and added alternate solution using object literal

--- solution.js
+++ solution.js
@@ -1,7 +1,21 @@
-const updateLight = (current) => {
-    return current === 'green' || current !== 'yellow' ? current === 'red' ? 'green' : 'yellow' : 'red'
+const updateLight = current => {
+    return current === 'green' ? 'yellow' : current === 'yellow' ? 'red' : 'green'
 }
 
 console.log(updateLight('green')) // yellow
 console.log(updateLight('yellow')) // red
 console.log(updateLight('red')) // green
+
+
+// Alternate solution //
+
+
+const updateLights = current => ({
+    green: 'yellow',
+    red: 'green',
+    yellow: 'red',
+})[current]
+
+console.log(updateLights('green')) // yellow
+console.log(updateLights('yellow')) // red
+console.log(updateLights('red')) // green
